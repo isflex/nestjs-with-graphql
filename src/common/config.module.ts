ac8784d30943970bb01552bad19596d59ea8064c
ischerer <hello@flexiness.com>
2023-01-28 01:46:22 +0100

    init and update deps

--- src/common/config.module.ts
+++ src/common/config.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
+console.log('FLEX_MODE', process.env.FLEX_MODE)
+
 @Module({
   imports: [
     NestConfigModule.forRoot({
-      envFilePath: ['.env'],
+      envFilePath: [`../env/public/.env.${process.env.FLEX_MODE}`],
       isGlobal: true,
     }),
   ],
