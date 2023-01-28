0a6322addae8d99a08a01bd53d7495bd15814395
ischerer <hello@flexiness.com>
2023-01-28 01:04:41 +0100

    init and update deps

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(Number(process.env.FLEX_DATABASE_PORT), `${process.env.FLEX_DATABASE_HOSTNAME}`);
+  console.log(`[HTTP] : ${process.env.FLEX_DATABASE_HOST} :`, await app.getHttpServer().address());
+  console.log(`${process.env.FLEX_SERVER_RUNNING} ${process.env.FLEX_DATABASE_HOST}`)
 }
 bootstrap();
